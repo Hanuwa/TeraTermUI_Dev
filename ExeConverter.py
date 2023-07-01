1d1e1a812720466f7e639ad15555202b78c106e7
Hanuwa <109267068+Hanuwa@users.noreply.github.com>
2023-07-01 11:59:11 -0400

    Update ExeConverter.py

--- ExeConverter.py
+++ ExeConverter.py
@@ -3,6 +3,7 @@ import sqlite3
 import shutil
 import os
 import sys
+from colorama import init, Fore, Style
 
 project_directory = r"C:\Users\arman\PycharmProjects\TeraTermUI\TeraTermUI.py"
 nuitka_command = (
@@ -24,10 +25,11 @@ nuitka_command = (
     r'--include-data-dir=C:\Users\arman\PycharmProjects\TeraTermUI\venv\Lib\site-packages\spellchecker=spellchecker'
 )
 output_directory = r"C:\Users\arman\OneDrive\Documentos\TeraTermUI_v0.9.0"
+app_folder = "TeraTermUI"
 upx_command = r"upx --best " + output_directory + r"\TeraTermUI.dist\TeraTermUI.exe"
 inno_directory = r"C:\Program Files (x86)\Inno Setup 6\ISCC.exe"
 versions = ['installer', 'portable']
-
+init()
 
 connection = sqlite3.connect(r"C:\Users\arman\PycharmProjects\TeraTermUI\database.db")
 cursor = connection.cursor()
@@ -53,6 +55,7 @@ for version in versions:
                                 'archive = pyzipper.AESZipFile(self.ath)')
             data = data.replace('FileLock(lock_file, timeout=10)',
                                 'FileLock(lock_file_appdata, timeout=10)')
+            print(Fore.GREEN + "Successfully started installer version" + Style.RESET_ALL)
         else:
             script = "portable"
             data = data.replace('self.connection = sqlite3.connect(self.db_path)',
@@ -65,53 +68,62 @@ for version in versions:
                                 'archive = pyzipper.AESZipFile(self.SERVICE_ACCOUNT_FILE)')
             data = data.replace('FileLock(lock_file_appdata, timeout=10)',
                                 'FileLock(lock_file, timeout=10)')
+            print(Fore.GREEN + "Successfully started portable version" + Style.RESET_ALL)
 
         with open(project_directory, 'w', encoding='utf-8') as file:
             file.write(data)
     except Exception as e:
-        print(f"Error modifying script: {e}")
+        print(Fore.RED + f"Error modifying script: {e}" + Style.RESET_ALL)
         sys.exit(1)
 
     try:
         subprocess.run(nuitka_command, shell=True, check=True)
+        print(Fore.GREEN + "Successfully completed nuitka script" + Style.RESET_ALL)
     except Exception as e:
-        print(f"Failed to create executable with Nuitka: {e}")
+        print(Fore.RED + f"Failed to create executable with Nuitka: {e}" + Style.RESET_ALL)
         sys.exit(1)
 
     try:
         subprocess.run(upx_command, shell=True, check=True)
+        print(Fore.GREEN + "Successfully completed UPX script" + Style.RESET_ALL)
     except Exception as e:
-        print(f"Failed to compress executable with UPX: {e}")
+        print(Fore.RED + f"Failed to compress executable with UPX: {e}" + Style.RESET_ALL)
         sys.exit(1)
 
     if script == "installer":
         try:
             os.rename(output_directory + r"\TeraTermUI.dist",
                       output_directory + r"\TeraTermUI_installer")
+            print(Fore.GREEN + "Successfully created TeraTermUI installer folder" + Style.RESET_ALL)
         except Exception as e:
-            print(f"Error renaming folder: {e}")
+            print(Fore.RED + f"Error renaming folder: {e}" + Style.RESET_ALL)
             sys.exit(1)
 
         try:
             subprocess.run([inno_directory, output_directory + r"\InstallerScript.iss"], check=True)
+            print(Fore.GREEN + "Successfully compiled TeraTermUI installer" + Style.RESET_ALL)
         except Exception as e:
-            print(f"Error compiling Inno Setup script: {e}")
+            print(Fore.RED + f"Error compiling Inno Setup script: {e}" + Style.RESET_ALL)
             sys.exit(1)
 
         try:
             shutil.move(output_directory + r"\output\TeraTermUI_64-bit_Installer-v0.9.0.exe", output_directory)
             shutil.rmtree(output_directory + r"\output")
+            print(Fore.GREEN + "Successfully completed installer version" + Style.RESET_ALL)
         except Exception as e:
-            print(f"Error moving or removing folder: {e}")
+            print(Fore.RED + f"Error moving or removing folder: {e}" + Style.RESET_ALL)
             sys.exit(1)
 
     elif script == "portable":
         try:
             os.rename(output_directory + r"\TeraTermUI.dist", output_directory + r"\TeraTermUI")
-            shutil.make_archive(output_directory + r"\TeraTermUI-v0.9.0", 'zip', output_directory, "TeraTermUI")
+            zip_file_path = output_directory + fr"\{app_folder}-v0.9.0.zip"
+            shutil.make_archive(zip_file_path, 'zip', output_directory, app_folder)
+            print(Fore.GREEN + "Successfully completed portable version" + Style.RESET_ALL)
         except Exception as e:
-            print(f"Error creating zip file: {e}")
+            print(Fore.RED + f"Error creating zip file: {e}" + Style.RESET_ALL)
             sys.exit(1)
 
-print("Both versions (installer and portable) have been created successfully.")
+print(Fore.GREEN + "Both versions (installer and portable) have been created successfully."
+      + Style.RESET_ALL)
 sys.exit(0)
