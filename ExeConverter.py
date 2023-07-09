430ff213e6a4d9123a7ca36da271f1eabb3640ef
Hanuwa <109267068+Hanuwa@users.noreply.github.com>
2023-07-09 18:36:47 -0400

    Update ExeConverter.py

--- ExeConverter.py
+++ ExeConverter.py
@@ -5,40 +5,77 @@ import os
 import sys
 from colorama import init, Fore, Style
 
-project_directory = r"C:\Users\arman\PycharmProjects\TeraTermUI\TeraTermUI.py"
-nuitka_command = (
-    r'cd /d C:\Users\arman\PycharmProjects\TeraTermUI\venv\Scripts & python -m nuitka --standalone '
-    r'--experimental=treefree C:\Users\arman\PycharmProjects\TeraTermUI\TeraTermUI.py '
-    r'--enable-plugin=tk-inter --include-data-dir=C:\Users\arman\PycharmProjects\TeraTermUI\venv\Lib\site-packages'
-    r'\customtkinter=customtkinter --include-data-dir=C:\Users\arman\PycharmProjects\TeraTermUI\venv\Lib\site-packages'
-    r'\CTkMessageBox=CTkMessageBox --include-package=CTkToolTip '
-    r'--include-data-dir=C:\Users\arman\PycharmProjects\TeraTermUI\images=images '
-    r'--include-data-dir=C:\Users\arman\PycharmProjects\TeraTermUI\sounds=sounds '
-    r'--include-data-file=C:\Users\arman\PycharmProjects\TeraTermUI\database.db=database.db '
-    r'--include-data-file=C:\Users\arman\PycharmProjects\TeraTermUI\Tesseract-OCR.7z=Tesseract-OCR.7z '
-    r'--include-data-file=C:\Users\arman\PycharmProjects\TeraTermUI\feedback.zip=feedback.zip '
-    r'--include-data-file=C:\Users\arman\PycharmProjects\TeraTermUI\VERSION.txt=VERSION.txt '
-    r'--include-data-file=C:\Users\arman\PycharmProjects\TeraTermUI\LICENSE.txt=LICENSE.txt '
-    r'--output-dir=C:\Users\arman\OneDrive\Documentos\TeraTermUI_v0.9.0 --disable-console '
-    r'--windows-icon-from-ico=C:\Users\arman\PycharmProjects\TeraTermUI\images\tera-term.ico'
-)
-output_directory = r"C:\Users\arman\OneDrive\Documentos\TeraTermUI_v0.9.0"
-app_folder = "TeraTermUI"
-upx_command = r"upx --best " + output_directory + r"\TeraTermUI.dist\TeraTermUI.exe"
-inno_directory = r"C:\Program Files (x86)\Inno Setup 6\ISCC.exe"
-versions = ['installer', 'portable']
 init()
+username = os.getlogin()
+project_directory = r"C:\Users\\" + username + r"\PycharmProjects\TeraTermUI"
+inno_directory = r"C:\Program Files (x86)\Inno Setup 6\ISCC.exe"
+app_folder = "TeraTermUI"
+update = "v0.9.0"
+update_without_v = "0.9.0"
+versions = ['installer', 'portable']
+output_directory = os.path.join(r"C:/Users/" + username + "/OneDrive/Documentos", "TeraTermUI_" + update)
+upx_command = r"upx --best " + output_directory + r"\TeraTermUI.dist\TeraTermUI.exe"
+try:
+    os.makedirs(output_directory, exist_ok=True)
+    for filename in os.listdir(project_directory+"\dist"):
+        src = os.path.join(project_directory+"\dist", filename)
+        dst = os.path.join(output_directory, filename)
+        if os.path.isfile(src):
+            shutil.copy2(src, dst)
+    # Open the inno script to overwrite it
+    file_path = os.path.join(output_directory, "InstallerScript.iss")
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+    with open(file_path, "w") as file:
+        for line in lines:
+            if line.startswith("#define MyAppVersion"):
+                line = '#define MyAppVersion "' + update_without_v + '"\n'
+            elif line.startswith("#define MyAppPath"):
+                line = '#define MyAppPath "' + output_directory + '"\n'
+            elif line.startswith("OutputBaseFilename="):
+                line = 'OutputBaseFilename="TeraTermUI_64-bit_Installer-' + update + '"\n'
+            # Write the line back to the file
+            file.write(line)
+    print(Fore.GREEN + "Successfully created distribution directory\n" + Style.RESET_ALL)
+except Exception as e:
+    print(Fore.RED + f"Error modifying creating distribution directory: {e}\n" + Style.RESET_ALL)
+    sys.exit(1)
+nuitka_command = (
+    r'cd /d "'+project_directory+r'\venv\Scripts" & python -m nuitka --standalone '
+    r'--experimental=treefree "'+project_directory+r'\TeraTermUI.py" '
+    r'--enable-plugin=tk-inter --include-data-dir="'+project_directory+r'\venv\Lib\site-packages'
+    r'\customtkinter=customtkinter" --include-data-dir="'+project_directory+r'\venv\Lib\site-packages'
+    r'\CTkMessageBox=CTkMessageBox" --include-package=CTkToolTip '
+    r'--include-data-dir="'+project_directory+r'\images=images" '
+    r'--include-data-dir="'+project_directory+r'\sounds=sounds" '
+    r'--include-data-file="'+project_directory+r'\database.db=database.db" '
+    r'--include-data-file="'+project_directory+r'\Tesseract-OCR.7z=Tesseract-OCR.7z" '
+    r'--include-data-file="'+project_directory+r'\feedback.zip=feedback.zip" '
+    r'--include-data-file="'+project_directory+r'\VERSION.txt=VERSION.txt" '
+    r'--include-data-file="'+project_directory+r'\LICENSE.txt=LICENSE.txt" '
+    r'--output-dir="'+output_directory+r'" --disable-console '
+    r'--windows-icon-from-ico="'+project_directory+r'\images\tera-term.ico" '
+    r'--lto=yes '
+)
 
-connection = sqlite3.connect(r"C:\Users\arman\PycharmProjects\TeraTermUI\database.db")
-cursor = connection.cursor()
+
 # Execute the DELETE SQL command
+connection = sqlite3.connect(r"C:\Users" + "\\" + username + r"\PycharmProjects\TeraTermUI\database.db")
+cursor = connection.cursor()
 cursor.execute("DELETE FROM user_data")
 connection.commit()
+# Check current state of the script to decide the order of versions
+with open(project_directory+r"\TeraTermUI.py", 'r', encoding='utf-8') as file:
+    data = file.read()
+if 'self.connection = sqlite3.connect("database.db")' in data:
+    versions = ['installer', 'portable']
+else:
+    versions = ['portable', 'installer']
 
 for version in versions:
     script = None
     try:
-        with open(project_directory, 'r', encoding='utf-8') as file:
+        with open(project_directory+r"\TeraTermUI.py", 'r', encoding='utf-8') as file:
             data = file.read()
         # Replace old text with new text for portable and installer versions
         if version == 'installer':
@@ -53,7 +90,7 @@ for version in versions:
                                 'archive = pyzipper.AESZipFile(self.ath)')
             data = data.replace('FileLock(lock_file, timeout=10)',
                                 'FileLock(lock_file_appdata, timeout=10)')
-            print(Fore.GREEN + "Successfully started installer version" + Style.RESET_ALL)
+            print(Fore.GREEN + "Successfully started installer version\n" + Style.RESET_ALL)
         else:
             script = "portable"
             data = data.replace('self.connection = sqlite3.connect(self.db_path)',
@@ -66,62 +103,62 @@ for version in versions:
                                 'archive = pyzipper.AESZipFile(self.SERVICE_ACCOUNT_FILE)')
             data = data.replace('FileLock(lock_file_appdata, timeout=10)',
                                 'FileLock(lock_file, timeout=10)')
-            print(Fore.GREEN + "Successfully started portable version" + Style.RESET_ALL)
+            print(Fore.GREEN + "Successfully started portable version\n" + Style.RESET_ALL)
 
-        with open(project_directory, 'w', encoding='utf-8') as file:
+        with open(project_directory+r"\TeraTermUI.py", 'w', encoding='utf-8') as file:
             file.write(data)
     except Exception as e:
-        print(Fore.RED + f"Error modifying script: {e}" + Style.RESET_ALL)
+        print(Fore.RED + f"Error modifying script: {e}\n" + Style.RESET_ALL)
         sys.exit(1)
 
     try:
         subprocess.run(nuitka_command, shell=True, check=True)
-        print(Fore.GREEN + "Successfully completed nuitka script" + Style.RESET_ALL)
+        print(Fore.GREEN + "Successfully completed nuitka script\n" + Style.RESET_ALL)
     except Exception as e:
-        print(Fore.RED + f"Failed to create executable with Nuitka: {e}" + Style.RESET_ALL)
+        print(Fore.RED + f"Failed to create executable with Nuitka: {e}\n" + Style.RESET_ALL)
         sys.exit(1)
 
     try:
         subprocess.run(upx_command, shell=True, check=True)
-        print(Fore.GREEN + "Successfully completed UPX script" + Style.RESET_ALL)
+        print(Fore.GREEN + "Successfully completed UPX script\n" + Style.RESET_ALL)
     except Exception as e:
-        print(Fore.RED + f"Failed to compress executable with UPX: {e}" + Style.RESET_ALL)
+        print(Fore.RED + f"Failed to compress executable with UPX: {e}\n" + Style.RESET_ALL)
         sys.exit(1)
 
     if script == "installer":
         try:
             os.rename(output_directory + r"\TeraTermUI.dist",
                       output_directory + r"\TeraTermUI_installer")
-            print(Fore.GREEN + "Successfully created TeraTermUI installer folder" + Style.RESET_ALL)
+            print(Fore.GREEN + "Successfully created TeraTermUI installer folder\n" + Style.RESET_ALL)
         except Exception as e:
-            print(Fore.RED + f"Error renaming folder: {e}" + Style.RESET_ALL)
+            print(Fore.RED + f"Error renaming folder: {e}\n" + Style.RESET_ALL)
             sys.exit(1)
 
         try:
             subprocess.run([inno_directory, output_directory + r"\InstallerScript.iss"], check=True)
-            print(Fore.GREEN + "Successfully compiled TeraTermUI installer" + Style.RESET_ALL)
+            print(Fore.GREEN + "Successfully compiled TeraTermUI installer\n" + Style.RESET_ALL)
         except Exception as e:
-            print(Fore.RED + f"Error compiling Inno Setup script: {e}" + Style.RESET_ALL)
+            print(Fore.RED + f"Error compiling Inno Setup script: {e}\n" + Style.RESET_ALL)
             sys.exit(1)
 
         try:
-            shutil.move(output_directory + r"\output\TeraTermUI_64-bit_Installer-v0.9.0.exe", output_directory)
+            shutil.move(output_directory + r"\output\TeraTermUI_64-bit_Installer-"+update+".exe", output_directory)
             shutil.rmtree(output_directory + r"\output")
-            print(Fore.GREEN + "Successfully completed installer version" + Style.RESET_ALL)
+            print(Fore.GREEN + "Successfully completed installer version\n" + Style.RESET_ALL)
         except Exception as e:
-            print(Fore.RED + f"Error moving or removing folder: {e}" + Style.RESET_ALL)
+            print(Fore.RED + f"Error moving or removing folder: {e}\n" + Style.RESET_ALL)
             sys.exit(1)
 
     elif script == "portable":
         try:
             os.rename(output_directory + r"\TeraTermUI.dist", output_directory + r"\TeraTermUI")
-            zip_file_path = output_directory + fr"\{app_folder}-v0.9.0"
+            zip_file_path = output_directory + fr"\{app_folder}-"+update+""
             shutil.make_archive(zip_file_path, 'zip', output_directory, app_folder)
-            print(Fore.GREEN + "Successfully completed portable version" + Style.RESET_ALL)
+            print(Fore.GREEN + "Successfully completed portable version\n" + Style.RESET_ALL)
         except Exception as e:
-            print(Fore.RED + f"Error creating zip file: {e}" + Style.RESET_ALL)
+            print(Fore.RED + f"Error creating zip file: {e}\n" + Style.RESET_ALL)
             sys.exit(1)
 
-print(Fore.GREEN + "Both versions (installer and portable) have been created successfully."
+print(Fore.GREEN + "Both versions (installer and portable) have been created successfully.\n"
       + Style.RESET_ALL)
 sys.exit(0)
