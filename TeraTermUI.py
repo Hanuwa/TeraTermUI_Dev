cb607074dbeada8e734f70933acf4b1d0ce20477
Hanuwa <109267068+Hanuwa@users.noreply.github.com>
2023-10-06 14:11:42 -0400

    Update TeraTermUI.py

--- TeraTermUI.py
+++ TeraTermUI.py
@@ -5,7 +5,7 @@
 # DESCRIPTION - Controls The application called Tera Term through a GUI interface to make the process of
 # enrolling classes for the university of Puerto Rico at Bayamon easier
 
-# DATE - Started 1/1/23, Current Build v0.9.0 - 10/3/23
+# DATE - Started 1/1/23, Current Build v0.9.0 - 10/6/23
 
 # BUGS / ISSUES - The implementation of pytesseract could be improved, it sometimes fails to read the screen properly,
 # depends a lot on the user's system and takes a bit time to process.
@@ -3733,6 +3733,7 @@ class TeraTermUI(customtkinter.CTk):
                 instructor = match.group(7)
                 instructor = re.sub(r'\bN\b', '', instructor)  # remove standalone 'N'
                 instructor = re.sub(r'\bFULL\b', '', instructor)  # remove standalone 'FULL'
+                instructor = re.sub(r'\bRSVD\b', '', instructor)  # remove standalone 'RSVD'
                 instructor = instructor.strip()  # remove leading and trailing whitespace
                 data.append({
                     "SEC": match.group(1),
@@ -4087,54 +4088,64 @@ class TeraTermUI(customtkinter.CTk):
 
     # links to each correspondant curriculum that the user chooses
     def curriculums(self, choice):
-        self.focus_set()
-        if choice == "Departments" or choice == "Departamentos":
-            webbrowser.open("https://www.uprb.edu/sample-page/decanato-de-asuntos-academicos/"
-                            "departamentos-academicos-2/")
-        if choice == "Accounting" or choice == "Contabilidad":
-            webbrowser.open("https://drive.google.com/file/d/0BzdErxfu_JSCSDA0NHMyYVNhdXA3V1ZqX2c1aUlIT21Oc1RF/view?"
-                            "resourcekey=0-S2WGur2snYQ0UVIHABbdKg")
-        elif choice == "Finance" or choice == "Finanzas":
-            webbrowser.open("https://drive.google.com/file/d/0BzdErxfu_JSCR2gyNzJOeHA2c2EwTklRYmZYZ0Zfck9UT3E0/view?"
-                            "resourcekey=0-jizC_JvFrbYxmb9ZScl8RA")
-        elif choice == "Management" or choice == "Gerencia":
-            webbrowser.open("https://drive.google.com/file/d/0BzdErxfu_JSCVllhTWJGMzRYd3JoemtObDkzX3I5MHNqU3V3/view?"
-                            "resourcekey=0-368G697L5iz5EjZ_DCngHQ")
-        elif choice == "Marketing" or choice == "Mercadeo":
-            webbrowser.open("https://drive.google.com/file/d/0BzdErxfu_JSCa3BIWnZyQmlHa0hGcEVtSlV2d2gxN0dENVcw/view?"
-                            "resourcekey=0-hve5FwLHcBdt0K6Je5hMSg")
-        elif choice == "General Biology" or choice == "Biología General":
-            webbrowser.open("https://drive.google.com/file/d/11yfoYqXYPybDZmeEmgW8osgSCCmxzjQl/view")
-        elif choice == "Biology-Human Focus" or choice == "Biología-Enfoque Humano":
-            webbrowser.open("https://drive.google.com/file/d/1z-aphTwLLwAY5-G3O7_SXG3ZvvRSN6p9/view")
-        elif choice == "Computer Science" or choice == "Ciencias de Computadoras":
-            webbrowser.open("https://docs.uprb.edu/deptsici/CIENCIAS-DE-COMPUTADORAS-2016.pdf")
-        elif choice == "Information Systems" or choice == "Sistemas de Información":
-            webbrowser.open("https://docs.uprb.edu/deptsici/SISTEMAS-INFORMACION-2016.pdf")
-        elif choice == "Social Sciences" or choice == "Ciencias Sociales":
-            webbrowser.open("https://drive.google.com/file/d/1cZnD6EhBsu7u6U8IVZoeK0VHgQmYt3sf/view")
-        elif choice == "Physical Education" or choice == "Educación Física":
-            webbrowser.open("https://drive.google.com/file/d/0BzdErxfu_JSCQWFEWlpCSnRFMVFGQnZoTXRyZHJiMzBkc2dZ/view?"
-                            "resourcekey=0-zLsz0IP1Ajy853kM9I2PQg")
-        elif choice == "Electronics":
-            webbrowser.open("https://drive.google.com/file/d/1tfzaHKilu5iQccD2sBzD8O_6UlXtSREF/view")
-        elif choice == "Electrónica":
-            webbrowser.open("https://drive.google.com/file/d/1tfzaHKilu5iQccD2sBzD8O_6UlXtSREF/view")
-        elif choice == "Equipment Management" or choice == "Gerencia de Materiales":
-            webbrowser.open("https://drive.google.com/file/d/13ohtab5ns6qO2QIHouScKtrFHrM7X3zl/view")
-        elif choice == "Pedagogy" or choice == "Pedagogía":
-            webbrowser.open("https://www.upr.edu/bayamon/wp-content/uploads/sites/9/2015/06/"
-                            "Secuencia-curricular-aprobada-en-mayo-de-2013.pdf")
-        elif choice == "Chemistry" or choice == "Química":
-            webbrowser.open("https://drive.google.com/file/d/0BzdErxfu_JSCNHJENWNaY1JmZjNSSU5mR2U5SnVOc1gxUTVJ/view?"
-                            "resourcekey=0-CWkQQfEczPuV0Rx4KQnkBA")
-        elif choice == "Nursing" or choice == "Enfermería":
-            webbrowser.open("https://drive.google.com/file/d/0BzdErxfu_JSCaF9tMFc3Y0hnRGpsZ1dMTXFPRjRMUlVEQ1ZZ/view?"
-                            "resourcekey=0-JQUivKyxJQlXP-2K008d_Q")
-        elif choice == "Office Systems" or choice == "Sistemas de Oficina":
-            webbrowser.open("https://docs.uprb.edu/deptsofi/curriculo-BA-SOFI-agosto-2016.pdf")
-        elif choice == "Information Engineering" or choice == "Ingeniería de la Información":
-            webbrowser.open("https://drive.google.com/file/d/1mYCHmCy3Mb2fDyp9EiFEtR0j4-rsDdlN/view")
+        links = {
+            "Departments": "https://www.uprb.edu/sample-page/decanato-de-asuntos-academicos/departamentos-academicos-2/",
+            "Departamentos": "https://www.uprb.edu/sample-page/decanato-de-asuntos-academicos/departamentos"
+                             "-academicos-2/",
+            "Accounting": "https://drive.google.com/file/d/0BzdErxfu_JSCSDA0NHMyYVNhdXA3V1ZqX2c1aUlIT21Oc1RF/view"
+                          "?resourcekey=0-S2WGur2snYQ0UVIHABbdKg",
+            "Contabilidad": "https://drive.google.com/file/d/0BzdErxfu_JSCSDA0NHMyYVNhdXA3V1ZqX2c1aUlIT21Oc1RF/view"
+                            "?resourcekey=0-S2WGur2snYQ0UVIHABbdKg",
+            "Finance": "https://drive.google.com/file/d/0BzdErxfu_JSCR2gyNzJOeHA2c2EwTklRYmZYZ0Zfck9UT3E0/view"
+                       "?resourcekey=0-jizC_JvFrbYxmb9ZScl8RA",
+            "Finanzas": "https://drive.google.com/file/d/0BzdErxfu_JSCR2gyNzJOeHA2c2EwTklRYmZYZ0Zfck9UT3E0/view"
+                        "?resourcekey=0-jizC_JvFrbYxmb9ZScl8RA",
+            "Management": "https://drive.google.com/file/d/0BzdErxfu_JSCVllhTWJGMzRYd3JoemtObDkzX3I5MHNqU3V3/view"
+                          "?resourcekey=0-368G697L5iz5EjZ_DCngHQ",
+            "Gerencia": "https://drive.google.com/file/d/0BzdErxfu_JSCVllhTWJGMzRYd3JoemtObDkzX3I5MHNqU3V3/view"
+                        "?resourcekey=0-368G697L5iz5EjZ_DCngHQ",
+            "Marketing": "https://drive.google.com/file/d/0BzdErxfu_JSCa3BIWnZyQmlHa0hGcEVtSlV2d2gxN0dENVcw/view"
+                         "?resourcekey=0-hve5FwLHcBdt0K6Je5hMSg",
+            "Mercadeo": "https://drive.google.com/file/d/0BzdErxfu_JSCa3BIWnZyQmlHa0hGcEVtSlV2d2gxN0dENVcw/view"
+                        "?resourcekey=0-hve5FwLHcBdt0K6Je5hMSg",
+            "General Biology": "https://drive.google.com/file/d/11yfoYqXYPybDZmeEmgW8osgSCCmxzjQl/view",
+            "Biología General": "https://drive.google.com/file/d/11yfoYqXYPybDZmeEmgW8osgSCCmxzjQl/view",
+            "Biology-Human Focus": "https://drive.google.com/file/d/1z-aphTwLLwAY5-G3O7_SXG3ZvvRSN6p9/view",
+            "Biología-Enfoque Humano": "https://drive.google.com/file/d/1z-aphTwLLwAY5-G3O7_SXG3ZvvRSN6p9/view",
+            "Computer Science": "https://docs.uprb.edu/deptsici/CIENCIAS-DE-COMPUTADORAS-2016.pdf",
+            "Ciencias de Computadoras": "https://docs.uprb.edu/deptsici/CIENCIAS-DE-COMPUTADORAS-2016.pdf",
+            "Information Systems": "https://docs.uprb.edu/deptsici/SISTEMAS-INFORMACION-2016.pdf",
+            "Sistemas de Información": "https://docs.uprb.edu/deptsici/SISTEMAS-INFORMACION-2016.pdf",
+            "Social Sciences": "https://drive.google.com/file/d/1cZnD6EhBsu7u6U8IVZoeK0VHgQmYt3sf/view",
+            "Ciencias Sociales": "https://drive.google.com/file/d/1cZnD6EhBsu7u6U8IVZoeK0VHgQmYt3sf/view",
+            "Physical Education": "https://drive.google.com/file/d/0BzdErxfu_JSCQWFEWlpCSnRFMVFGQnZoTXRyZHJiMzBkc2dZ"
+                                  "/view?resourcekey=0-zLsz0IP1Ajy853kM9I2PQg",
+            "Educación Física": "https://drive.google.com/file/d/0BzdErxfu_JSCQWFEWlpCSnRFMVFGQnZoTXRyZHJiMzBkc2dZ/view"
+                                "?resourcekey=0-zLsz0IP1Ajy853kM9I2PQg",
+            "Electronics": "https://drive.google.com/file/d/1tfzaHKilu5iQccD2sBzD8O_6UlXtSREF/view",
+            "Electrónica": "https://drive.google.com/file/d/1tfzaHKilu5iQccD2sBzD8O_6UlXtSREF/view",
+            "Equipment Management": "https://drive.google.com/file/d/13ohtab5ns6qO2QIHouScKtrFHrM7X3zl/view",
+            "Gerencia de Materiales": "https://drive.google.com/file/d/13ohtab5ns6qO2QIHouScKtrFHrM7X3zl/view",
+            "Pedagogy": "https://www.upr.edu/bayamon/wp-content/uploads/sites/9/2015/06/Secuencia-curricular-aprobada"
+                        "-en-mayo-de-2013.pdf",
+            "Pedagogía": "https://www.upr.edu/bayamon/wp-content/uploads/sites/9/2015/06/Secuencia-curricular"
+                         "-aprobada-en-mayo-de-2013.pdf",
+            "Chemistry": "https://drive.google.com/file/d/0BzdErxfu_JSCNHJENWNaY1JmZjNSSU5mR2U5SnVOc1gxUTVJ/view"
+                         "?resourcekey=0-CWkQQfEczPuV0Rx4KQnkBA",
+            "Química": "https://drive.google.com/file/d/0BzdErxfu_JSCNHJENWNaY1JmZjNSSU5mR2U5SnVOc1gxUTVJ/view"
+                       "?resourcekey=0-CWkQQfEczPuV0Rx4KQnkBA",
+            "Nursing": "https://drive.google.com/file/d/0BzdErxfu_JSCaF9tMFc3Y0hnRGpsZ1dMTXFPRjRMUlVEQ1ZZ/view"
+                       "?resourcekey=0-JQUivKyxJQlXP-2K008d_Q",
+            "Enfermería": "https://drive.google.com/file/d/0BzdErxfu_JSCaF9tMFc3Y0hnRGpsZ1dMTXFPRjRMUlVEQ1ZZ/view"
+                          "?resourcekey=0-JQUivKyxJQlXP-2K008d_Q",
+            "Office Systems": "https://docs.uprb.edu/deptsofi/curriculo-BA-SOFI-agosto-2016.pdf",
+            "Sistemas de Oficina": "https://docs.uprb.edu/deptsofi/curriculo-BA-SOFI-agosto-2016.pdf",
+            "Information Engineering": "https://drive.google.com/file/d/1mYCHmCy3Mb2fDyp9EiFEtR0j4-rsDdlN/view",
+            "Ingeniería de la Información": "https://drive.google.com/file/d/1mYCHmCy3Mb2fDyp9EiFEtR0j4-rsDdlN/view"}
+
+        url = links.get(choice, None)
+        if url:
+            webbrowser.open(url)
 
     # will tell the user that there's a new update available for the application
     def update_app(self):
