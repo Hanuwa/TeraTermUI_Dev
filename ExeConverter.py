d6d4184e51de96de6a828926cf77dd7db37a440c
Hanuwa <109267068+Hanuwa@users.noreply.github.com>
2023-10-09 22:47:16 -0400

    Update ExeConverter.py

--- ExeConverter.py
+++ ExeConverter.py
@@ -115,6 +115,7 @@ nuitka_command = (
     r'--include-data-file="'+project_directory+r'\spanish.json=spanish.json" '                                                
     r'--output-dir="'+output_directory+r'" --disable-console '
     r'--windows-icon-from-ico="'+project_directory+r'\images\tera-term.ico" '
+    r'--include-package=websockets.legacy '
     r'--lto=yes --nofollow-import-to=unittest --nofollow-import-to=reportlab.graphics.testshapes'
 )
 
