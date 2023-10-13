8f3a44ba0fb3117612b6fa1db53a324257740ee9
Hanuwa <109267068+Hanuwa@users.noreply.github.com>
2023-10-12 20:15:43 -0400

    Update ExeConverter.py

--- ExeConverter.py
+++ ExeConverter.py
@@ -39,9 +39,6 @@ def check_and_restore_backup():
         program_backup_date = extract_second_date_from_file(program_backup)
         tera_term_ui_date = extract_second_date_from_file(project_directory + r"\TeraTermUI.py")
         tera_term_ui_version = extract_version_main_file(project_directory + r"\TeraTermUI.py")
-        print(program_backup_date)
-        print(tera_term_ui_date)
-        print(tera_term_ui_version)
         if program_backup_date != tera_term_ui_date:
             print(Fore.YELLOW + "\nDate mismatch detected between the main file and the backup one. "
                                 "\nRestoration from backup skipped. Delete backup file if no longer needed"
@@ -53,7 +50,7 @@ def check_and_restore_backup():
         else:
             shutil.copy2(program_backup, project_directory + r"\TeraTermUI.py")
             print(
-                Fore.YELLOW + "\nPrevious session was interrupted. Restoration from backup completed." + 
+                Fore.YELLOW + "\nPrevious session was interrupted. Restoration from backup completed." +
                 Style.RESET_ALL)
             os.remove(program_backup)
 
@@ -96,7 +93,7 @@ else:
     update = "v" + update_without_v
 versions = ['installer', 'portable']
 output_directory = os.path.join(r"C:/Users/" + username + "/OneDrive/Documentos", "TeraTermUI_" + update)
-upx_command = r"upx --ultra-brute " + output_directory + r"\TeraTermUI.dist\TeraTermUI.exe"
+upx_command = r"upx " + output_directory + r"\TeraTermUI.dist\TeraTermUI.exe"
 
 # If process gets abruptly interrupted, load backup file
 program_backup = project_directory + r"\TeraTermUI.BAK.py"
