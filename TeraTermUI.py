27b0b977ced001a041d235219d52c60447b2a66f
Hanuwa <109267068+Hanuwa@users.noreply.github.com>
2024-01-22 10:59:11 -0400

    Update TeraTermUI.py

--- TeraTermUI.py
+++ TeraTermUI.py
@@ -5,7 +5,7 @@
 # DESCRIPTION - Controls The application called Tera Term through a GUI interface to make the process of
 # enrolling classes for the university of Puerto Rico at Bayamon easier
 
-# DATE - Started 1/1/23, Current Build v0.9.0 - 1/21/24
+# DATE - Started 1/1/23, Current Build v0.9.0 - 1/22/24
 
 # BUGS / ISSUES - The implementation of pytesseract could be improved, it sometimes fails to read the screen properly,
 # depends a lot on the user's system and takes a bit time to process.
@@ -3399,7 +3399,7 @@ class TeraTermUI(customtkinter.CTk):
                        translation["days"], translation["times"], translation["av"],
                        translation["instructor"]]
 
-        for display_class, table, semester, show in self.class_table_pairs:
+        for _, table, _, _, _ in self.class_table_pairs:
             table.update_headers(new_headers)
             for i, new_header in enumerate(new_headers):
                 tooltip_message = tooltip_messages[new_header]
