74d2d19573010df059cbcb6457e14b5cefaf44ed
Hanuwa <109267068+Hanuwa@users.noreply.github.com>
2024-02-19 03:33:24 -0400

    Update TeraTermUI.py

--- TeraTermUI.py
+++ TeraTermUI.py
@@ -5,7 +5,7 @@
 # DESCRIPTION - Controls The application called Tera Term through a GUI interface to make the process of
 # enrolling classes for the university of Puerto Rico at Bayamon easier
 
-# DATE - Started 1/1/23, Current Build v0.9.0 - 2/18/24
+# DATE - Started 1/1/23, Current Build v0.9.0 - 2/19/24
 
 # BUGS / ISSUES - The implementation of pytesseract could be improved, it sometimes fails to read the screen properly,
 # depends a lot on the user's system and takes a bit time to process.
@@ -6308,6 +6308,7 @@ class TeraTermUI(customtkinter.CTk):
 
     # checks whether the program can continue its normal execution or if the server is on maintenance
     def wait_for_prompt(self, prompt_text, maintenance_text, timeout=15):
+        time.sleep(1)
         start_time = time.time()
         while True:
             text_output = self.capture_screenshot()
