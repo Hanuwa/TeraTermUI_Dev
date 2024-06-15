a5e99c31a8796f16e96b6d7af5da39e7d30e0065
Hanuwa <109267068+Hanuwa@users.noreply.github.com>
2024-06-15 15:23:04 -0400

    Update TeraTermUI.py

--- TeraTermUI.py
+++ TeraTermUI.py
@@ -5,7 +5,7 @@
 # DESCRIPTION - Controls The application called Tera Term through a GUI interface to make the process of
 # enrolling classes for the university of Puerto Rico at Bayamon easier
 
-# DATE - Started 1/1/23, Current Build v0.9.5 - 6/14/24
+# DATE - Started 1/1/23, Current Build v0.9.5 - 6/15/24
 
 # BUGS / ISSUES - The implementation of pytesseract could be improved, it sometimes fails to read the screen properly,
 # depends a lot on the user's system and takes a bit time to process.
@@ -89,7 +89,7 @@ from PIL import Image
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 warnings.filterwarnings("ignore", message="32-bit application should be automated using 32-bit Python")
-gc.set_threshold(2100, 40, 20)
+gc.set_threshold(3500, 100, 100)
 
 
 def measure_time(threshold):
