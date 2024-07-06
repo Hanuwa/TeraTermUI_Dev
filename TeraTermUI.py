d4ec901f0d4925e7bb392987f93cb1fe6e5d1269
Hanuwa <109267068+Hanuwa@users.noreply.github.com>
2024-07-06 17:24:00 -0400

    Update TeraTermUI.py

--- TeraTermUI.py
+++ TeraTermUI.py
@@ -5,7 +5,7 @@
 # DESCRIPTION - Controls The application called Tera Term through a GUI interface to make the process of
 # enrolling classes for the university of Puerto Rico at Bayamon easier
 
-# DATE - Started 1/1/23, Current Build v0.9.5 - 7/5/24
+# DATE - Started 1/1/23, Current Build v0.9.5 - 7/6/24
 
 # BUGS / ISSUES - The implementation of pytesseract could be improved, it sometimes fails to read the screen properly,
 # depends a lot on the user's system and takes a bit time to process.
@@ -4503,6 +4503,7 @@ class TeraTermUI(customtkinter.CTk):
     def end_countdown(self):
         lang = self.language_menu.get()
         translation = self.load_language(lang)
+        self.pr_date = None
         self.auto_enroll_bool = False
         self.countdown_running = False
         self.notification_sent = False
