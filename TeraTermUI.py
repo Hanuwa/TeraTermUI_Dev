a2b8e2df2e4adfa12cf14487d92ff8c6cdd64068
Hanuwa <109267068+Hanuwa@users.noreply.github.com>
2024-09-05 21:36:55 -0400

    Update TeraTermUI.py

--- TeraTermUI.py
+++ TeraTermUI.py
@@ -5,7 +5,7 @@
 # DESCRIPTION - Controls The application called Tera Term through a GUI interface to make the process of
 # enrolling classes for the university of Puerto Rico at Bayamon easier
 
-# DATE - Started 1/1/23, Current Build v0.9.5 - 9/3/24
+# DATE - Started 1/1/23, Current Build v0.9.5 - 9/5/24
 
 # BUGS / ISSUES - The implementation of pytesseract could be improved, it sometimes fails to read the screen properly,
 # depends a lot on the user's system and takes a bit time to process.
@@ -11068,9 +11068,10 @@ class ImageSlideshow(customtkinter.CTkFrame):
 
     def load_images(self):
         image_files = [f for f in os.listdir(self.image_folder) if f.endswith(("png", "jpg", "jpeg"))]
+        random.seed(time.time())
         random.shuffle(image_files)
         self.image_files = image_files
-
+        
     def show_image(self):
         # Delete the previous image from memory
         if hasattr(self, "current_image"):
