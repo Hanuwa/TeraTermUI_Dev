c9d26195b12c48f42990c96904ff71c012fc4e38
Hanuwa <109267068+Hanuwa@users.noreply.github.com>
2024-09-15 20:40:02 -0400

    Update ExecutableScript.bat

--- ExecutableScript.bat
+++ ExecutableScript.bat
@@ -4,7 +4,7 @@
 REM  --> Check for permissions
 >nul 2>&1 "%SYSTEMROOT%\system32\icacls.exe" "%SYSTEMROOT%\system32\config\system"
 
-REM --> If error flag set, we do not have admin.
+REM --> If error flag set, we do not have admin
 if '%errorlevel%' NEQ '0' (
     echo Requesting administrative privileges...
     timeout /t 2 /nobreak >nul
@@ -12,8 +12,10 @@ if '%errorlevel%' NEQ '0' (
 ) else ( goto gotAdmin )
 
 :UACPrompt
+    setlocal
+    set "args=%*"
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "%args%", "", "runas", 1 >> "%temp%\getadmin.vbs"
 
     "%temp%\getadmin.vbs"
     exit /B
@@ -44,8 +46,9 @@ if '%errorlevel%' NEQ '0' (
 echo.
 
 :RunScript
-:: Run the Python script
-python ExeConverter.py
+:: Run the Python script and pass all command-line arguments
+python ExeConverter.py %*
+
 if '%errorlevel%' NEQ '0' (
     echo Failed to run ExeConverter.py. Please check the script for errors.
     pause
