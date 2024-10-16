25887b6af8d7406df246fc25f55521cf4dcc9376
Hanuwa <109267068+Hanuwa@users.noreply.github.com>
2024-10-16 01:43:49 -0400

    Update TeraTermUI.py

--- TeraTermUI.py
+++ TeraTermUI.py
@@ -5,7 +5,7 @@
 # DESCRIPTION - Controls The application called Tera Term through a GUI interface to make the process of
 # enrolling classes for the university of Puerto Rico at Bayamon easier
 
-# DATE - Started 1/1/23, Current Build v0.9.5 - 10/15/24
+# DATE - Started 1/1/23, Current Build v0.9.5 - 10/16/24
 
 # BUGS / ISSUES - The implementation of pytesseract could be improved, it sometimes fails to read the screen properly,
 # depends a lot on the user's system and takes a bit time to process.
