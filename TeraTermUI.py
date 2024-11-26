72c6aa73dc5688e6f010e06a5c3c8c1f22e9e34e
Hanuwa <109267068+Hanuwa@users.noreply.github.com>
2024-11-26 01:19:48 -0400

    Update TeraTermUI.py

--- TeraTermUI.py
+++ TeraTermUI.py
@@ -5,7 +5,7 @@
 # DESCRIPTION - Controls The application called Tera Term through a GUI interface to make the process of
 # enrolling classes for the university of Puerto Rico at Bayamon easier
 
-# DATE - Started 1/1/23, Current Build v0.9.5 - 11/25/24
+# DATE - Started 1/1/23, Current Build v0.9.5 - 11/26/24
 
 # BUGS / ISSUES - The implementation of pytesseract could be improved, it sometimes fails to read the screen properly,
 # depends a lot on the user's system and takes a bit time to process.
@@ -11921,7 +11921,7 @@ def main():
     language_id = ctypes.windll.kernel32.GetUserDefaultUILanguage()
     os.chdir(os.path.dirname(sys.argv[0]))
     temporary_dir = os.path.join(tempfile.gettempdir(), "TeraTermUI")
-    lock_file = os.path.join( temporary_dir, "TeraTermUI_Updater.lock")
+    lock_file = os.path.join(temporary_dir, "TeraTermUI_Updater.lock")
     if os.path.exists(lock_file):
         logging.error(f"The updater is currently running. Application cannot be launched")
         sys.exit(1)
