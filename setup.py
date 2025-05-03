d0903c7c62214e8423012cd74305de0ad1228563
Hanuwa <109267068+Hanuwa@users.noreply.github.com>
2025-05-03 13:59:01 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import winreg
 
 PYTHON_REQUIRED = (3, 12, 10)
 MSVC_URL = "https://aka.ms/vs/17/release/vs_BuildTools.exe"
-INNO_SETUP_URL = "https://files.jrsoftware.org/is/6/innosetup-6.4.2.exe"
+INNO_SETUP_URL = "https://files.jrsoftware.org/is/6/innosetup-6.4.3.exe"
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 VENV_DIR = os.path.join(ROOT_DIR, ".venv")
