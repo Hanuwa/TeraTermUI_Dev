b38c408fb1f14e2990ae48bb406f4943b29aeb9f
Hanuwa <109267068+Hanuwa@users.noreply.github.com>
2025-07-12 13:45:48 -0400

    Update ExecutableScript.bat

--- ExecutableScript.bat
+++ ExecutableScript.bat
@@ -16,7 +16,6 @@ if '%errorlevel%' NEQ '0' (
     set "args=%*"
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
     echo UAC.ShellExecute "%~s0", "%args%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
     "%temp%\getadmin.vbs"
     exit /B
 
@@ -35,16 +34,21 @@ if not exist "%CD%\.venv\Scripts\activate.bat" (
 :: Activate the virtual environment
 call "%CD%\.venv\Scripts\activate.bat"
 
-:: Check if colorama is installed in the virtual environment
+:: Check if colorama is installed
 python -c "import colorama" >nul 2>&1
 if '%errorlevel%' NEQ '0' (
     goto InstallColorama
-) else (
-    goto RunScript
 )
 
+:: Check if pywin32 is installed
+python -c "import win32api" >nul 2>&1
+if '%errorlevel%' NEQ '0' (
+    goto InstallPywin32
+)
+
+goto RunScript
+
 :InstallColorama
-:: Install colorama in the virtual environment
 echo Installing colorama...
 pip install colorama
 if '%errorlevel%' NEQ '0' (
@@ -54,14 +58,21 @@ if '%errorlevel%' NEQ '0' (
 )
 echo.
 
-:RunScript
-:: Run the Python script and pass all command-line arguments
-python ExeConverter.py %*
+:InstallPywin32
+echo Installing pywin32...
+pip install pywin32
+if '%errorlevel%' NEQ '0' (
+    echo Failed to install pywin32. Please install it manually
+    pause
+    exit /B
+)
+echo.
 
+:RunScript
+python ExeConverter.py %*
 if '%errorlevel%' NEQ '0' (
     echo Failed to run ExeConverter.py. Please check the script for errors
     pause
     exit /B
 )
-
 pause
